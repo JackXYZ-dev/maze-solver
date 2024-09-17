d21fa676117e58f90734bd7216ea4b11d2004d3f
jackXYZ-dev <jackrobertsxyz@pm.me>
2024-09-17 09:39:21 +0100

    Draw lines

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from window import Window
+from window import *
 
 
 def main():
     win = Window(800, 600)
+    test_point = Point(10, 10)
+    test_point2 = Point(500, 500)
+    test_line = Line(test_point, test_point2)
+    win.draw_line(test_line, "black")
     win.wait_for_close()
 
 
